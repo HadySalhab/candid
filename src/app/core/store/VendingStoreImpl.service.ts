4c59e22bb78a11b3667d39cbdcd19a8da9fabc75
Hady Salhab <hadysalhab1@gmail.com>
2021-09-22 09:45:32 +1000

    add getStateChange

--- src/app/core/store/VendingStoreImpl.service.ts
+++ src/app/core/store/VendingStoreImpl.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { StoreState } from '../model/StoreState';
 import { VendingStore } from './VendingStore.service';
 
 @Injectable()
 export class VendingStoreImpl extends VendingStore {
+  getStateChange(): Observable<StoreState> {
+    return this.stateChanged;
+  }
   updateState(newState: StoreState, action: string): void {
     this.setState(newState, action);
     console.log(this.stateHistory);
