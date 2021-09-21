36cc2c6a5c2109ba1bef36def244d59d3cc0968b
Hady Salhab <hadysalhab1@gmail.com>
2021-09-22 00:16:30 +1000

    setup vending buttons

---
+++ src/app/vending-machine/vending-button/vending-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vending-button',
+  templateUrl: './vending-button.component.html',
+  styleUrls: ['./vending-button.component.scss']
+})
+export class VendingButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/vending-machine/vending-machine.component.ts
+++ src/app/vending-machine/vending-machine.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { map, share } from 'rxjs/operators';
+import { map, share, shareReplay, tap } from 'rxjs/operators';
 
 import { Observable } from 'rxjs';
 import { StoreState } from '@app/core/model/StoreState';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { Summary } from './summary-screen/summary.model';
+import { VendingItem } from '@app/core/model/VendingItem';
 import { VendingService } from '@app/core/services/vending.service';
 import { VendingStore } from '@app/core/store/VendingStore';
 
@@ -14,13 +15,21 @@ import { VendingStore } from '@app/core/store/VendingStore';
 })
 export class VendingMachineComponent implements OnInit {
   summary$: Observable<Summary>;
+  vendingItems$: Observable<VendingItem[]>;
   storeState$: Observable<StoreState>;
   constructor(private vs: VendingService, private store: VendingStore) {}
 
   ngOnInit(): void {
-    this.storeState$ = this.store.stateChanged.pipe(share());
+    this.storeState$ = this.store.stateChanged.pipe(shareReplay());
+    this.vendingItems$ = this.storeState$.pipe(
+      map((state) => {
+        console.warn(state);
+        return [...state.vendingItems];
+      })
+    );
     this.summary$ = this.storeState$.pipe(
       map((state) => {
+        console.warn(state);
         return {
           totalCansSold: state.totalCansSold,
           totalCashAmount: state.totalCashAmount,
