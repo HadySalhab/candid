f108f013f0e5047f9f3e19310ca06a121fa6fc46
Hady Salhab <hadysalhab1@gmail.com>
2021-09-21 19:39:43 +1000

    create input module

--- src/app/shared/controls/input/input.component.ts
+++ src/app/shared/controls/input/input.component.ts
@@ -1,18 +1,65 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  EventEmitter,
+  Input,
   OnInit,
+  Output,
   ViewEncapsulation,
+  forwardRef,
 } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
   encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputComponent),
+      multi: true,
+    },
+  ],
 })
-export class InputComponent implements OnInit {
+export class InputComponent implements ControlValueAccessor {
+  @Input() placeholder: string = '';
+  @Output() changed: EventEmitter<number> = new EventEmitter();
+
+  value: number = 0;
+  isDisabled: boolean = false;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  private propagateChange: any = () => {};
+  private propagateTouched: any = () => {};
+
+  writeValue(value: number): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propagateTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onKeyup(input: HTMLInputElement): void {
+    const value = parseInt(input.value);
+    this.propagateChange(value);
+    this.changed.emit(value);
+  }
+
+  onBlur(): void {
+    this.propagateTouched();
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { InputComponent } from './controls/input/input.component';
+import { InputModule } from './controls/input/input.module';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [InputComponent],
-  exports: [InputComponent],
-  imports: [CommonModule],
+  declarations: [],
+  exports: [InputModule, ReactiveFormsModule],
+  imports: [CommonModule, InputModule],
 })
 export class SharedModule {}
