200c9529f14b4e7108434f20d190f7fd696df17d
Hady Salhab <hadysalhab1@gmail.com>
2021-09-21 20:04:25 +1000

    create select module

---
+++ src/app/shared/controls/select/select.component.ts
@@ -0,0 +1,66 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewEncapsulation,
+  forwardRef,
+} from '@angular/core';
+import { ControlItem, Value } from '../control-item.model';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-select',
+  templateUrl: './select.component.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true,
+    },
+  ],
+})
+export class SelectComponent implements OnInit, ControlValueAccessor {
+  constructor() {}
+
+  @Input() items: ControlItem[] = [];
+  @Output() changed = new EventEmitter<Value>();
+
+  value?: Value;
+  isDisabled: boolean = false;
+
+  private propagateChange: any = () => {};
+  private propagateTouched: any = () => {};
+
+  writeValue(value: Value): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propagateTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onChanged(selectEl: HTMLSelectElement): void {
+    const value = selectEl.value ? selectEl.value : null;
+    this.propagateChange(value);
+    this.changed.emit(value);
+  }
+
+  onBlur(): void {
+    this.propagateTouched();
+  }
+
+  ngOnInit(): void {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { InputComponent } from './controls/input/input.component';
 import { InputModule } from './controls/input/input.module';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SelectModule } from './controls/select/select.module';
 
 @NgModule({
   declarations: [],
-  exports: [InputModule, ReactiveFormsModule],
-  imports: [CommonModule, InputModule],
+  exports: [InputModule, SelectModule, ReactiveFormsModule],
+  imports: [CommonModule, InputModule, SelectModule],
 })
 export class SharedModule {}
