35039ffc6e26fe512bb68814463bb5698fc369ba
Hady Salhab <hadysalhab1@gmail.com>
2021-09-22 11:17:14 +1000

    map vending items to vending buttons

--- src/app/vending-machine/vending-button/vending-button.component.ts
+++ src/app/vending-machine/vending-button/vending-button.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { VendingItem } from '@app/core/model/VendingItem';
 
 @Component({
   selector: 'app-vending-button',
   templateUrl: './vending-button.component.html',
-  styleUrls: ['./vending-button.component.scss']
+  styleUrls: ['./vending-button.component.scss'],
 })
 export class VendingButtonComponent implements OnInit {
+  @Input()
+  vendingItem: VendingItem;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
